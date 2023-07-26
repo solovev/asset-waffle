366648f3c766714f3ce241242f09149883b03638
solovev <mrbroume@gmail.com>
2023-07-26 09:06:03 +0400

    comms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  // Fix mantine.ui styles: https://github.com/orgs/mantinedev/discussions/1672#discussioncomment-5922089
   corePlugins: { preflight: false },
   theme: {
     extend: {},
